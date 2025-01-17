a2cdcae5e671af5d52984eaf78797aab329f87b3
HenryLi-0 <bxsci8266@gmail.com>
2025-01-17 16:15:46 -0500

    WOWOOWOWOWOOO

--- src/main/java/org/sciborgs1155/dashboard/Main.java
+++
@@ -1,5 +0,0 @@
-package org.sciborgs1155.dashboard;
-
-class Main {
-  public static void main(String args[]) {}
-}
---
+++ src/main/java/org/sciborgs1155/dashboard/Program.java
@@ -0,0 +1,38 @@
+package org.sciborgs1155.dashboard;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+import java.io.IOException;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.util.WPIUtilJNI;
+public class Program {
+    public static void main(String[] args) throws IOException {
+        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+        WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
+        WPIMathJNI.Helper.setExtractOnStaticLoad(false);
+        CameraServerJNI.Helper.setExtractOnStaticLoad(false);
+        CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni");
+        new Program().run();
+    }
+    public void run() {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable table = inst.getTable("Robot");
+        NetworkTableEntry entry = table.getEntry("speedMultiplier");
+        inst.startClient4("jkasdflk");
+        inst.setServer("localhost");
+        inst.startDSClient();
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                System.out.println("interrupted");
+                return;
+            }
+            entry.setDouble(Math.random());
+        }
+    }
+}
