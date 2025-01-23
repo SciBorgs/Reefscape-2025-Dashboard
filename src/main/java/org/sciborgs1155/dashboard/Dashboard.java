c3470930192ca3de66f2b2fee28b5517e4f49828
HenryLi-0 <bxsci8266@gmail.com>
2025-01-23 18:08:59 -0500

    rename images files (what resolution  are your displays?)

--- src/main/java/org/sciborgs1155/dashboard/Dashboard.java
+++ src/main/java/org/sciborgs1155/dashboard/Dashboard.java
@@ -68,7 +68,7 @@ public class Dashboard {
 
     JLabel bgImage;
     try {
-      BufferedImage buffer = ImageIO.read(getClass().getResource("images/bluebg.png"));
+      BufferedImage buffer = ImageIO.read(getClass().getResource("images/bg_blue.png"));
       bgImage = new JLabel(new ImageIcon(buffer));
       bgImage.setBounds(-20, -15, 1498, 861);
     } catch (Exception e) {
@@ -76,18 +76,18 @@ public class Dashboard {
     }
     this.bgImage = bgImage;
 
-    SA = bindButtonWithBranch("A", "images/As.png", 602, 647, 126, 153);
-    SB = bindButtonWithBranch("B", "images/Bm.png", 742, 647, 126, 153);
-    SC = bindButtonWithBranch("C", "images/Ct.png", 915, 573, 124, 151);
-    SE = bindButtonWithBranch("D", "images/Dw.png", 1048, 421, 126, 152);
-    SF = bindButtonWithBranch("E", "images/Ev.png", 1010, 251, 124, 152);
-    SD = bindButtonWithBranch("F", "images/Fg.png", 917, 97, 126, 151);
-    SG = bindButtonWithBranch("G", "images/Go.png", 742, 31, 122, 152);
-    SH = bindButtonWithBranch("H", "images/Hl.png", 601, 31, 126, 151);
-    SI = bindButtonWithBranch("I", "images/Ik.png", 420, 97, 126, 152);
-    SJ = bindButtonWithBranch("J", "images/Jp.png", 328, 254, 125, 152);
-    SK = bindButtonWithBranch("K", "images/Ks.png", 278, 421, 124, 152);
-    SL = bindButtonWithBranch("L", "images/Lp.png", 411, 573, 124, 152);
+    SA = bindButtonWithBranch("A", "images/br_As.png", 602, 647, 126, 153);
+    SB = bindButtonWithBranch("B", "images/br_Bm.png", 742, 647, 126, 153);
+    SC = bindButtonWithBranch("C", "images/br_Ct.png", 915, 573, 124, 151);
+    SE = bindButtonWithBranch("D", "images/br_Dw.png", 1048, 421, 126, 152);
+    SF = bindButtonWithBranch("E", "images/br_Ev.png", 1010, 251, 124, 152);
+    SD = bindButtonWithBranch("F", "images/br_Fg.png", 917, 97, 126, 151);
+    SG = bindButtonWithBranch("G", "images/br_Go.png", 742, 31, 122, 152);
+    SH = bindButtonWithBranch("H", "images/br_Hl.png", 601, 31, 126, 151);
+    SI = bindButtonWithBranch("I", "images/br_Ik.png", 420, 97, 126, 152);
+    SJ = bindButtonWithBranch("J", "images/br_Jp.png", 328, 254, 125, 152);
+    SK = bindButtonWithBranch("K", "images/br_Ks.png", 278, 421, 124, 152);
+    SL = bindButtonWithBranch("L", "images/br_Lp.png", 411, 573, 124, 152);
 
     branches = List.of(SA, SB, SC, SD, SE, SF, SG, SH, SI, SJ, SK, SL);
     processor = bindButton("images/Pg.png", 1309, 6, 147, 185);
@@ -109,13 +109,13 @@ public class Dashboard {
           }
         });
 
-    L1 = bindButton("images/l1.png", 1333, 532, 103, 99);
+    L1 = bindButton("images/lb_L1.png", 1333, 532, 103, 99);
     JButton l1Button = (JButton) panel.getComponent(panel.getComponentCount() - 1);
-    L2 = bindButton("images/L2.png", 1333, 428, 103, 99);
+    L2 = bindButton("images/lb_L2.png", 1333, 428, 103, 99);
     JButton l2Button = (JButton) panel.getComponent(panel.getComponentCount() - 1);
-    L3 = bindButton("images/L3.png", 1333, 322, 103, 99);
+    L3 = bindButton("images/lb_L3.png", 1333, 322, 103, 99);
     JButton l3Button = (JButton) panel.getComponent(panel.getComponentCount() - 1);
-    L4 = bindButton("images/L4.png", 1333, 219, 103, 99);
+    L4 = bindButton("images/lb_L4.png", 1333, 219, 103, 99);
     JButton l4Button = (JButton) panel.getComponent(panel.getComponentCount() - 1);
     levels = List.of(L1, L2, L3, L4);
 
@@ -157,7 +157,7 @@ public class Dashboard {
     redButton = (JButton) panel.getComponent(panel.getComponentCount() - 1);
     redButton.addActionListener(
         e -> {
-          changeBackground("images/redbg.png", redButton);
+          changeBackground("images/bg_red.png", redButton);
         });
     redButton.setBorderPainted(false);
 
@@ -165,7 +165,7 @@ public class Dashboard {
     blueButton = (JButton) panel.getComponent(panel.getComponentCount() - 1);
     blueButton.addActionListener(
         e -> {
-          changeBackground("images/bluebg.png", blueButton);
+          changeBackground("images/bg_blue.png", blueButton);
         });
     blueButton.setBorderPainted(false);
 
@@ -231,21 +231,21 @@ public class Dashboard {
     try {
       BufferedImage buffer = ImageIO.read(getClass().getResource(imgPath));
       bgImage.setIcon(new ImageIcon(buffer));
-      if (imgPath.contains("bluebg")) {
+      if (imgPath.contains("bg_blue")) {
         bgColor = new Color(0, 131, 174); // Blue background
         displayLabel.setForeground(bgColor);
         levelsButtons
             .get(0)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/l1.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lb_L1.png"))));
         levelsButtons
             .get(1)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/L2.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lb_L2.png"))));
         levelsButtons
             .get(2)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/L3.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lb_L3.png"))));
         levelsButtons
             .get(3)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/L4.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lb_L4.png"))));
         goButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/goo.png"))));
         redButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/red.png"))));
         blueButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/blue.png"))));
@@ -253,28 +253,28 @@ public class Dashboard {
             .setIcon(
                 new ImageIcon(
                     ImageIO.read(getClass().getResource("images/rst.png")))); // Reset button
-      } else if (imgPath.contains("redbg")) {
+      } else if (imgPath.contains("bg_red")) {
         bgColor = new Color(197, 45, 60); // Red background
         displayLabel.setForeground(bgColor);
         levelsButtons
             .get(0)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rL1.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lr_L1.png"))));
         levelsButtons
             .get(1)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rL2.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lr_L2.png"))));
         levelsButtons
             .get(2)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rL3.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lr_L3.png"))));
         levelsButtons
             .get(3)
-            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rL4.png"))));
+            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/lr_L4.png"))));
         goButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rgoo.png"))));
         redButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rred.png"))));
         blueButton.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("images/rblue.png"))));
         ((JButton) panel.getComponent(panel.getComponentCount() - 4))
             .setIcon(
                 new ImageIcon(
-                    ImageIO.read(getClass().getResource("images/rrst.png")))); // Reset button
+                    ImageIO.read(getClass().getResource("images/r_reset.png")))); // Reset button
       }
       panel.setBackground(bgColor);
     } catch (Exception e) {
