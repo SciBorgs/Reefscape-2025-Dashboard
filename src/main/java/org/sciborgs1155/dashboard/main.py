e823466c32e5cddc07b0e8cebf512cf44ad2a972
HenryLi-0 <bxsci8266@gmail.com>
2025-01-25 13:28:28 -0500

    and here it begins

--- src/main/java/org/sciborgs1155/dashboard/main.py
+++ src/main/java/org/sciborgs1155/dashboard/main.py
@@ -1,5 +1,8 @@
 import ntcore
 import time
+
+from constants import *
+
 if __name__ == "__main__":
     inst = ntcore.NetworkTableInstance.getDefault()
     table = inst.getTable("Dashboard")
@@ -7,10 +10,15 @@ if __name__ == "__main__":
     entryTick = table.getEntry("tick")
     tick = 0
 
-    inst.startClient4("example client")
-    inst.setServer("localhost")
-    # inst.setServerTeam(team=1155)
-    # inst.startDSClient()
+    inst.startClient4("Dashboard")
+
+    if REAL:
+        inst.setServerTeam(team=1155, port=5810)
+        inst.startDSClient()
+    else:
+        inst.setServer("localhost")
+
+
     while True:
         time.sleep(0.02)
         entryTick.setInteger(tick)
