077bdbc5571ecc9e000a84dff17d158d91720685
Akk5250 <Akkumar52505250@gmail.com>
2025-01-25 15:13:32 -0500

    Finished initialization of 'Comms' class

--- subsystems/comms.py
+++ subsystems/comms.py
@@ -1,27 +1,56 @@
-import ntcore
 import time
+
+import ntcore
+
 from settings import *
 
+
 class Comms:
-    def __init__(self):
-        
-        inst = ntcore.NetworkTableInstance.getDefault()
-        table = inst.getTable("Dashboard")
+    '''Handles communication between the program and network tables'''
+    def __init__(self) -> None:
+        instance = ntcore.NetworkTableInstance.getDefault()
+        instance.startClient4(identity="Dashboard")
 
-        entryTick = table.getEntry("tick")
-        tick = 0
-
-        inst.startClient4("Dashboard")
+        self.networkTable = instance.getTable(key="Dashboard")
 
         if REAL:
-            inst.setServerTeam(team=1155, port=5810)
-            inst.startDSClient()
+            instance.setServerTeam(team=1155, port=5810)
+            instance.startDSClient()
         else:
-            inst.setServer("localhost")
+            instance.setServer(server_name="localhost")
 
+        # Entry for the target branch to score on
+        self.targetBranch = self.networkTable.getEntry(key="branch")
+        self.targetBranch.setString(value="")
 
-        while True:
-            time.sleep(0.02)
-            entryTick.setInteger(tick)
-            tick+=1
-            print("boop")
+        # Entry for the target level to score on
+        self.targetLevel = self.networkTable.getEntry(key="level")
+        self.targetLevel.setInteger(value=0)
+
+        # Entry for the status of the connection
+        self.isConnected = self.networkTable.getEntry(key="robotConnected")
+        self.isConnected.setBoolean(value=True)
+
+        # Entry for the robot's alliance
+        self.alliance = self.networkTable.getEntry(key="blueAlliance")
+        self.alliance.setBoolean(value=True)
+
+        # Entry for whether to score processor or not
+        self.scoringProcessor = self.networkTable.getEntry(key="processor")
+        self.scoringProcessor.setBoolean(value=False)
+
+        # Entry for the closest branch
+        self.closestBranch = self.networkTable.getEntry(key="closestBranch")
+        self.closestBranch.setString(value="")
+
+        # Tracks update ticks
+        self.tick = self.networkTable.getEntry(key="tick")
+        self.tick.setInteger(value=0)
+
+    def update(self) -> None:
+        '''Meant to be called periodically'''
+        time.sleep(0.02)
+        self.tick.setInteger(value=self.tick.getInteger(defaultValue=0) + 1)
+        
+        
+    
