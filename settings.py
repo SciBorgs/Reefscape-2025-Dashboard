45ddef924711025900ae56900ac0ad041c27ba0f
HenryLi-0 <bxsci8266@gmail.com>
2025-01-31 14:59:19 -0500

    small occasional refresh timing change

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ STIMULATION = [
 INTERFACE_FPS = 60 # The desired operating FPS
 FPS_DAMPENING = 1 # The number of seconds between FPS calculations
 TICK_MS = 1 # Extra delay between frames, must be 1 or greater
-OCCASIONAL_TICK_MS = 5000 # Should keep above 1 second, as it runs processes that do not need updates every tick
+OCCASIONAL_TICK_MS = 1000 # Should keep above 1 second, as it runs processes that do not need updates every tick
 
 hexColorToRGBA = lambda hexcolor: tuple(int(hexcolor[i:i+2], 16) for i in (1, 3, 5)) + (255,)
 
