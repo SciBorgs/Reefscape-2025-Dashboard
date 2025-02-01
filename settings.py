b580d46503dc123261ed6c278f9ab4f92660517c
Akk5250 <Akkumar52505250@gmail.com>
2025-02-01 13:00:25 -0500

    'interface.py' and 'window.py' mostly Done. Just 'visuals.py' left.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ TOUCHSCREEN = True
 COMMS = True
 
 # Sets which modes allow stimulation devices to display. (Note: Stimulation devices affect performance!)
-STIMULATION = [
+STIMULATION: list[str] = [
     "disconnected",
     # "red",
     # "blue",
@@ -32,40 +32,43 @@ FPS_DAMPENING = 1 # The number of seconds between FPS calculations
 TICK_MS = 1 # Extra delay between frames, must be 1 or greater
 OCCASIONAL_TICK_MS = 1000 # Should keep above 1 second, as it runs processes that do not need updates every tick
 
-hexColorToRGBA = lambda hexcolor: tuple(int(hexcolor[i:i+2], 16) for i in (1, 3, 5)) + (255,)
+def hexColorToRGBA(hexcolor: str) -> tuple[*tuple[int, ...]]:
+    return tuple(int(hexcolor[i:i+2], 16) for i in (1, 3, 5)) + (255,)
 
 BACKGROUND_COLOR = "#000000" #Background color
-BACKGROUND_COLOR_RGBA = hexColorToRGBA(BACKGROUND_COLOR)
+BACKGROUND_COLOR_RGBA: tuple[int, ...] = hexColorToRGBA(hexcolor=BACKGROUND_COLOR)
 
 
 import os
 from PIL import ImageFont
+from PIL.Image import Image
 from subsystems.lib.simplefancy import *
 from subsystems.lib.render import *
 
 # Version
 VERSION = "v0.0.0"
-SYS_IVOS = [-999,-998,-997,-996]
+SYS_IVOS: list[int] = [-999,-998,-997,-996]
 
 
 # Fonts
-FONTS_ALL = ["Orbitron-VariableFont_wght.ttf"]
-FONT_PATH = os.path.join("resources", "fonts", FONTS_ALL[0])
-FONT_LARGE = ImageFont.truetype(FONT_PATH, 35)
-FONT_MEDIUM = ImageFont.truetype(FONT_PATH, 15)
-FONT_SMALL_MEDIUM = ImageFont.truetype(FONT_PATH, 12)
-FONT_SMALL = ImageFont.truetype(FONT_PATH, 10)
-EDITOR_SPACING = lambda x: x*20+15
+FONTS_ALL: list[str] = ["Orbitron-VariableFont_wght.ttf"]
+FONT_PATH: str = os.path.join("resources", "fonts", FONTS_ALL[0])
+FONT_LARGE: ImageFont.FreeTypeFont = ImageFont.truetype(font=FONT_PATH, size=35)
+FONT_MEDIUM: ImageFont.FreeTypeFont = ImageFont.truetype(font=FONT_PATH, size=15)
+FONT_SMALL_MEDIUM: ImageFont.FreeTypeFont = ImageFont.truetype(font=FONT_PATH, size=12)
+FONT_SMALL: ImageFont.FreeTypeFont = ImageFont.truetype(font=FONT_PATH, size=10)
+def EDITOR_SPACING(x: float) -> float:
+    return x*20+15
 
 # Images
-TEST_IMAGE = getImageRGBAFromPath(os.path.join("resources", "test.png"))
-CONNECTED_ALLIANCE_BACKGROUND = getImageRGBAFromPath(os.path.join("resources", "bg_connected.png"))
-DISCONNECTED_ALLIANCE_BACKGROUND = getImageRGBAFromPath(os.path.join("resources", "bg_disconnected.png"))
-SUBWAY_GIF = Image.open(os.path.join("resources", "subway guy.gif"))
+TEST_IMAGE: Image.Image = getImageRGBAFromPath(path=os.path.join("resources", "test.png"))
+CONNECTED_ALLIANCE_BACKGROUND: Image.Image = getImageRGBAFromPath(path=os.path.join("resources", "bg_connected.png"))
+DISCONNECTED_ALLIANCE_BACKGROUND: Image.Image = getImageRGBAFromPath(path=os.path.join("resources", "bg_disconnected.png"))
+SUBWAY_GIF: GifImageFile = Image.open(fp=os.path.join("resources", "subway guy.gif")) # type: ignore
 
 # Sections
 '''
 Region ID : Top Left, Bottom Right, Size, Keep In Relative Top Left, Keep In Relative Top Right
 '''
-SECTION_DATA = [(   0,   0),(1500, 865),(1500, 865),(   0,   0),(1500, 865)]
-SECTION_FRAME_INSTRUCTIONS = [[DISCONNECTED_ALLIANCE_BACKGROUND,(0,0)], [CONNECTED_ALLIANCE_BACKGROUND,(0,0)]]
+SECTION_DATA: list[tuple[int, int]] = [(   0,   0),(1500, 865),(1500, 865),(   0,   0),(1500, 865)]
+SECTION_FRAME_INSTRUCTIONS: list[tuple[Image.Image, tuple[int, int]]] = [(DISCONNECTED_ALLIANCE_BACKGROUND,(0,0)), (CONNECTED_ALLIANCE_BACKGROUND,(0,0))]
--- subsystems/interface.py
+++ subsystems/interface.py
@@ -1,26 +1,29 @@
 '''This file is all about managing what the user sees'''
 
+from typing import LiteralString
+
+from PIL.Image import Image
+from PIL.ImageTk import PhotoImage
 from settings import *
-from PIL import ImageTk, Image
-from tkinter import filedialog
-import time, random, ast
+import time
 from subsystems.lib.render import *
 from subsystems.lib.fancy import *
 from subsystems.lib.simplefancy import *
 from subsystems.lib.visuals import *
 from subsystems.lib.counter import Counter
 from subsystems.comms import *
+from subsystems.lib.visuals import BranchButtonVisualObject, DummyVisualObject
 
 class Interface:
-    def __init__(self):
-        self.mx = 0
-        self.my = 0
-        self.prevmx = 0
-        self.prevmy = 0
-        self.mPressed = False
-        self.mRising = False
-        self.fps = 0
-        self.ticks = 0
+    def __init__(self) -> None:
+        self.mx: int = 0
+        self.my: int = 0
+        self.prevmx: float = 0
+        self.prevmy: float = 0
+        self.mPressed: bool = False
+        self.mRising: bool = False
+        self.fps: float = 0
+        self.ticks: int = 0
         self.c = Counter()
         self.comms = Comms()
         '''Interactable Visual Objects'''
@@ -28,45 +31,45 @@ class Interface:
         Code:
         a - entire screen
         '''
-        self.ivos = {
-            -999 : [" ", DummyVisualObject("dummy", (0,0))], # used for not interacting with anything
-            -998 : [" ", DummyVisualObject("dummy", (0,0))], # used for text boxes
-            -997 : [" ", DummyVisualObject("dummy", (0,0))], # used by keybinds
-            -996 : [" ", DummyVisualObject("dummy", (0,0))], # used by scrolling
+        self.ivos: dict[int, tuple[str, VisualObject]] = {
+            -999 : (" ", DummyVisualObject(name="dummy", pos=(0,0))), # used for not interacting with anything
+            -998 : (" ", DummyVisualObject(name="dummy", pos=(0,0))), # used for text boxes
+            -997 : (" ", DummyVisualObject(name="dummy", pos=(0,0))), # used by keybinds
+            -996 : (" ", DummyVisualObject(name="dummy", pos=(0,0))), # used by scrolling
 
             # Example Usage
-            -50 : ["a", BranchButtonVisualObject("go", (111, 865/2-55), "GO")],
-            -49 : ["a", BranchButtonVisualObject("processor", (111, 865/2+55), "PS")],
+            -50 : ("a", BranchButtonVisualObject(name="go", pos=(111, 865/2-55), branch="GO")),
+            -49 : ("a", BranchButtonVisualObject(name="processor", pos=(111, 865/2+55), branch="PS")),
         }
 
         for i in range(12):
-            id = "ABCDEFGHIJKL"[i]
-            self.ivos[i] = ["a", BranchButtonVisualObject("Branch " + id, (round(math.cos(math.pi * i / 6 - 7 * math.pi /12) * 350) + 1500/2, round(math.sin(math.pi * i / 6 - 7 * math.pi /12) * -350) + 865/2), id)]
+            id: LiteralString = "ABCDEFGHIJKL"[i]
+            self.ivos[i] = ("a", BranchButtonVisualObject(name="Branch " + id, pos=(round(number=math.cos(math.pi * i / 6 - 7 * math.pi /12) * 350) + 1500/2, round(number=math.sin(math.pi * i / 6 - 7 * math.pi /12) * -350) + 865/2), branch=id))
 
         for i in range(1,5):
-            self.ivos[i+50] = ["a",BranchButtonVisualObject("Level " + str(i),(1389, (3-i-0.5)*(110)+865/2),"L" + str(i))]
+            self.ivos[i+50] = ("a",BranchButtonVisualObject(name="Level " + str(object=i),pos=(1389, (3-i-0.5)*(110)+865/2),branch="L" + str(object=i)))
 
 
         '''Control'''
-        self.interacting = -999
-        self.previousInteracting = -999
-        self.stringKeyQueue = ""
-        self.previousKeyQueue = []
-        self.mouseScroll = 0 
-        self.consoleAlerts = []
-        self.keybindLastUpdate = time.time()
-        self.lastTouchScreenPress = 0
+        self.interacting: int = -999
+        self.previousInteracting: int = -999
+        self.stringKeyQueue: Literal[''] = ""
+        self.previousKeyQueue: list[str] = []
+        self.mouseScroll: float = 0 
+        self.consoleAlerts: list[str] = []
+        self.keybindLastUpdate: float = time.time()
+        self.lastTouchScreenPress: float = 0
         '''Sliders'''
-        self.sliders = []
-        self.slidersData = []
+        self.sliders: list[Any] = []
+        self.slidersData: list[Any] = []
         '''DASHBOARD STUFF'''
-        self.selectedBranch = " "
-        self.selectedLevel = 0
-        self.alliance = ""
+        self.selectedBranch: str = " "
+        self.selectedLevel: int = 0
+        self.alliance: str = ""
         self.needUpdate = True
         pass
 
-    def tick(self,mx,my,mPressed,fps):
+    def tick(self,mx: int,my: int,mPressed: int,fps: float) -> None:
         '''Entire Screen: `(0,0) to (1499, 864)`: size `(1500, 865)`'''
         self.prevmx = self.mx
         self.prevmy = self.my
@@ -89,7 +92,7 @@ class Interface:
             self.mRising = mPressed==2
 
         self.fps = fps
-        self.deltaTicks = 1 if self.fps==0 else round(INTERFACE_FPS/self.fps)
+        self.deltaTicks: int = 1 if self.fps==0 else round(INTERFACE_FPS/self.fps)
         self.ticks += self.deltaTicks
 
         '''Interacting With...'''
@@ -99,14 +102,17 @@ class Interface:
         if self.interacting == -999 and self.mPressed and self.mRising:
             processed = False
             for id in self.ivos:
-                if self.ivos[id][1].getInteractable(self.mx - SECTION_DATA[0][0], self.my - SECTION_DATA[0][1]):
+                if self.ivos[id][1].getInteractable(rmx=self.mx - SECTION_DATA[0][0], rmy=self.my - SECTION_DATA[0][1]):
                     self.interacting = id
                     processed = True
                     break
                 if processed: break
         if self.interacting != -999:
-            self.ivos[self.interacting][1].updatePos(self.mx - SECTION_DATA[0][0], self.my - SECTION_DATA[0][1])
-            self.ivos[self.interacting][1].keepInFrame(SECTION_DATA[3][0],SECTION_DATA[3][1],SECTION_DATA[4][0],SECTION_DATA[4][1])
+            try:
+                self.ivos[self.interacting][1].updatePos(self.mx - SECTION_DATA[0][0], self.my - SECTION_DATA[0][1]) # type: ignore
+                self.ivos[self.interacting][1].keepInFrame(minX=SECTION_DATA[3][0],minY=SECTION_DATA[3][1],maxX=SECTION_DATA[4][0],maxY=SECTION_DATA[4][1]) # type: ignore
+            except:
+                pass
 
         '''DASHBOARD THINGS'''
         self.comms.tick()
@@ -114,7 +120,7 @@ class Interface:
             self.needUpdate = True
         if self.mRising:
             if 0 <= self.interacting <= 11:
-                new = "ABCDEFGHIJKL"[self.interacting]
+                new: str | int = "ABCDEFGHIJKL"[self.interacting]
                 if self.selectedBranch != new: self.selectedBranch = new
                 else: self.selectedBranch = " "
             elif 51 <= self.interacting <= 54:
@@ -129,26 +135,37 @@ class Interface:
                 self.selectedLevel = 0
             elif self.interacting == -50:
                 if self.selectedBranch == "processor":
-                    self.comms.setProcessor(True)
+                    self.comms.setProcessor(boolean=True)
                     self.selectedBranch = " "
                     self.selectedLevel = 0
                 elif self.selectedBranch != " " and self.selectedLevel != 0:
-                    self.comms.setProcessor(False)
-                    self.comms.setBranch(self.selectedBranch)
-                    self.comms.setLevel(self.selectedLevel)
+                    self.comms.setProcessor(boolean=False)
+                    self.comms.setBranch(branch=self.selectedBranch)
+                    self.comms.setLevel(level=self.selectedLevel)
                     self.selectedBranch = " "
                     self.selectedLevel = 0
                 else: pass
             else: pass
 
-        alliance = ("blue" if self.comms.getBlueAlliance() else "red") if self.comms.getIsConnected() else "disconnected"
+        alliance: Literal['blue','red','disconnected'] = ("blue" if self.comms.getBlueAlliance() else "red") if self.comms.getIsConnected() else "disconnected"
         if self.alliance != alliance:
             for i in range(0,11+1):
-                self.ivos[i][1].setAlliance(alliance)
+                try:
+                    self.ivos[i][1].setAlliance(alliance) # type: ignore
+                except:
+                    pass
             for i in range(51,54+1):
-                self.ivos[i][1].setAlliance(alliance)
-            self.ivos[-50][1].setAlliance(alliance)
-            self.ivos[-49][1].setAlliance(alliance)
+                try:
+                    self.ivos[i][1].setAlliance(alliance) # type: ignore
+                except:
+                    pass
+            
+            try:
+                self.ivos[-50][1].setAlliance(alliance) # type: ignore
+                self.ivos[-49][1].setAlliance(alliance) # type: ignore
+            except:
+                pass
+
             self.alliance = alliance
             self.needUpdate = True
         if alliance in STIMULATION:
@@ -156,51 +173,63 @@ class Interface:
 
 
 
-    def process(self, im):
-        img = im.copy()
+    def process(self, im : Image.Image) -> PhotoImage:
+        img: Image.Image = im.copy()
 
-        placeOver(img, displayText(f"FPS: {self.fps}", "m"), (20,20))
+        placeOver(img1=img, img2=displayText(text=f"FPS: {self.fps}", size="m"), position=(20,20))
         # placeOver(img, displayText(f"Interacting With: {self.interacting}", "m"), (20,55))
         # placeOver(img, displayText(f"length of IVO: {len(self.ivos)}", "m"), (20,90))
         # placeOver(img, displayText(f"Mouse Pos: ({self.mx}, {self.my})", "m"), (200,20))
         # placeOver(img, displayText(f"Mouse Press: {self.mPressed}", "m", colorTXT=(100,255,100,255) if self.mPressed else (255,100,100,255)), (200,55))
 
-        placeOver(img, displayText(f"Selected: {self.selectedBranch}{" " if self.selectedLevel==0 else self.selectedLevel}", "m"), (20,55))
+        placeOver(img1=img, img2=displayText(text=f"Selected: {self.selectedBranch}{" " if self.selectedLevel==0 else self.selectedLevel}", size="m"), position=(20,55))
         if not(COMMS):
-            placeOver(img, displayText(f"Comms has been disabled!", "m", colorTXT=(255,100,100,255)), (20,90))
+            placeOver(img1=img, img2=displayText(text=f"Comms has been disabled!", size="m", colorTXT=(255,100,100,255)), position=(20,90))
 
-        connected = self.comms.getIsConnected()
-        placeOver(img, displayText(f"Comms: Connected: {connected}", "m", colorTXT=(100,255,100,255) if connected else (255,100,100,255)), (20,775))
+        connected: bool = self.comms.getIsConnected()
+        placeOver(img1=img, img2=displayText(text=f"Comms: Connected: {connected}", size="m", colorTXT=(100,255,100,255) if connected else (255,100,100,255)), position=(20,775))
         if connected:
-            placeOver(img, displayText("Comms: Alliance: {}".format("Blue" if self.comms.getBlueAlliance() else "Red"), "m", colorTXT=(100,100,255,255) if self.comms.getBlueAlliance() else (255,100,100,255)), (20,800))
-            placeOver(img, displayText(f"Comms: Nearest: {self.comms.getNearest()}", "m"), (20,825))
+            placeOver(img1=img, img2=displayText(text="Comms: Alliance: {}".format("Blue" if self.comms.getBlueAlliance() else "Red"), size="m", colorTXT=(100,100,255,255) if self.comms.getBlueAlliance() else (255,100,100,255)), position=(20,800))
+            placeOver(img1=img, img2=displayText(text=f"Comms: Nearest: {self.comms.getNearest()}", size="m"), position=(20,825))
         else:
-            placeOver(img, displayText(f"Comms: Alliance: Disconnected", "m", colorTXT=(100,100,100,255)), (20,800))
-            placeOver(img, displayText(f"Comms: Nearest: Disconnected", "m", colorTXT=(100,100,100,255)), (20,825))
+            placeOver(img1=img, img2=displayText(text=f"Comms: Alliance: Disconnected", size="m", colorTXT=(100,100,100,255)), position=(20,800))
+            placeOver(img1=img, img2=displayText(text=f"Comms: Nearest: Disconnected", size="m", colorTXT=(100,100,100,255)), position=(20,825))
 
         if self.alliance in STIMULATION:
-            subway = getFrameFromGIF(SUBWAY_GIF, time.time()*16)
-            placeOver(img, subway, ( 230,  10))
-            placeOver(img, subway, (1130,  10))
-            placeOver(img, subway, ( 230, 615))
-            placeOver(img, subway, (1130, 615))
-            placeOver(img, subway, ( 670, 300))
+            subway: Image.Image = getFrameFromGIF(image=SUBWAY_GIF, frame=int(time.time()*16))
+            placeOver(img1=img, img2=subway, position=( 230,  10))
+            placeOver(img1=img, img2=subway, position=(1130,  10))
+            placeOver(img1=img, img2=subway, position=( 230, 615))
+            placeOver(img1=img, img2=subway, position=(1130, 615))
+            placeOver(img1=img, img2=subway, position=( 670, 300))
 
         for id in self.ivos:
             if self.ivos[id][0] == "a":
                 if 0 <= id <= 11:
-                    self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or id=="ABCDEFGHIJKL ".find(self.selectedBranch))
+                    try:
+                        self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or id=="ABCDEFGHIJKL ".find(self.selectedBranch)) # type: ignore
+                    except:
+                        pass
                 elif 51 <= id <= 54:
-                    self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or id==self.selectedLevel+50)
+                    try:
+                        self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or id==self.selectedLevel+50) # type: ignore
+                    except:
+                        pass
                 elif id == -49:
-                    self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or self.selectedBranch=="processor")
+                    try:
+                        self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id or self.selectedBranch=="processor") # type: ignore
+                    except:
+                        pass
                 else:
-                    self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id)
+                    try:
+                        self.ivos[id][1].tick(img, self.interacting==id, self.interacting==id) # type: ignore
+                    except:
+                        pass
 
-        return img    
+        return PhotoImage(image=img)
 
-    def saveState(self):
+    def saveState(self) -> None:
         pass
 
-    def close(self):
+    def close(self) -> None:
         pass
