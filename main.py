fc13d1d915f003b7c71f24a41a0621f52285a347
Akk5250 <Akkumar52505250@gmail.com>
2025-02-08 16:30:20 -0500

    Initial Commit(Welcome back to Gradle Hell! Home sweet home)

--- main.py
+++
@@ -1,8 +0,0 @@
-from subsystems.checks import Check
-Check.check()
-
-import tkinter as tk
-from subsystems.window import Window
-
-window = Window()
-window.start()
--- setup.bat
+++
@@ -1,3 +0,0 @@
-python.exe -m pip install pillow
-python.exe -m pip install numpy
-python.exe -m pip install robotpy
